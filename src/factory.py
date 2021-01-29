f37c6c23c6591473b6446d0f81f82a723678303c
Stark-X <xiaojiezhi2@gmail.com>
2021-01-29 11:17:03 +0800

    refactor: add remote_addr for logging

--- src/factory.py
+++ src/factory.py
@@ -1,3 +1,5 @@
+import logging
+from flask import has_request_context, request
 
 from flask import Flask
 from flask.logging import default_handler
@@ -16,11 +18,21 @@ def create_app():
     return app
 
 
+# noinspection StrFormat
+class EnhancedFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        record.url = None
+        record.remote_addr = None
+        if has_request_context():
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+        return super().format(record)
+
+
 def init_logger(app):
-    import logging
     log_format = app.config.get("LOG_FORMAT")
     if log_format is not None:
-        default_handler.setFormatter(logging.Formatter(log_format))
+        default_handler.setFormatter(EnhancedFormatter(log_format))
 
 
 def init_config(app):
