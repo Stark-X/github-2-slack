2fb646ed40a759ad928c9aef5cb4aca75c780b11
Stark-X <xiaojiezhi2@gmail.com>
2021-01-29 17:10:54 +0800

    refactor: accept str either in func get_signature

--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,9 @@ import hmac
 from typing import Union
 
 
-def get_signature(secret: str, payload: str) -> bytes:
-    key = bytes(secret, 'utf-8')
-    msg = bytes(payload, "utf-8")
+def get_signature(secret: Union[str, bytes], payload: Union[str, bytes]) -> bytes:
+    key = bytes(secret, "utf-8") if type(secret) == str else secret
+    msg = bytes(payload, "utf-8") if type(payload) == str else payload
     digester = hmac.new(key=key, msg=msg, digestmod=hashlib.sha256)
     signature = digester.hexdigest()
     return bytes(signature, "utf-8")
