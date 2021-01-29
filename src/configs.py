22648134b72538585cab04e426c0f9ca9ff01abf
Stark-X <xiaojiezhi2@gmail.com>
2021-01-29 17:08:17 +0800

    config: refinement

--- src/configs.py
+++ src/configs.py
@@ -2,3 +2,4 @@ class DefaultConfig(object):
     FLASK_ENV = "production"
     TESTING = False
     AUTH = True
+    LOG_FORMAT = "[%(asctime)s] [%(remote_addr)s] %(levelname)s in %(module)s.%(funcName)s(l:%(lineno)s): %(message)s"
